8afb6b65c6b2e85bfe397dd80d1d9a1b6f779da3
kimhongsuk <51160500+kimhongsuk@users.noreply.github.com>
2023-08-24 14:29:43 +0900

    Update start_openvswitch.sh

--- start_openvswitch.sh
+++ start_openvswitch.sh
@@ -2,8 +2,10 @@
 # start_openvswitch.sh
 
 # Open vSwitch Start
-cd ~/ovs
-sudo ovsdb-tool create /usr/local/etc/openvswitch/conf.db vswitchd/vswitch.ovsschema
+if [-e /usr/local/etc/openvswitch/conf.db]; then
+  cd ~/ovs
+  sudo ovsdb-tool create /usr/local/etc/openvswitch/conf.db vswitchd/vswitch.ovsschema
+fi
 sudo mkdir -p /usr/local/var/run/openvswitch
 export PATH=$PATH:/usr/local/share/openvswitch/scripts
 sudo ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock --remote=db:Open_vSwitch,Open_vSwitch,manager_options --private-key=db:Open_vSwitch,SSL,private_key --certificate=db:Open_vSwitch,SSL,certificate --bootstrap-ca-cert=db:Open_vSwitch,SSL,ca_cert --pidfile --detach --log-file
