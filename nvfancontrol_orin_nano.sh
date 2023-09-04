2ddb43cb71118e2c47dbc3b5e750b774f080db17
kimhongsuk <51160500+kimhongsuk@users.noreply.github.com>
2023-09-04 09:45:48 +0900

    clearance

--- nvfancontrol_orin_nano.sh
+++ nvfancontrol_orin_nano.sh
@@ -2,8 +2,7 @@
 # nvfancontrol.sh
 
 # Change fan-control strategy
-cd ~/set_avees_clustering
-sudo cp files/nvfancontrol_orin_nano.conf /etc/nvfancontrol.conf
+sudo cp ~/set_avees_clustering/files/nvfancontrol_orin_nano.conf /etc/nvfancontrol.conf
 sudo systemctl stop nvfancontrol
 sudo rm /var/lib/nvfancontrol/status
 sudo systemctl start nvfancontrol
